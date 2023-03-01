878252b435cb40a7ee60e46449470f2dd05af218
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-03-01 10:29:04 -0800

    Add frangi filter documentation and some comments

--- src/pipeline/frangi_filter.py
+++ src/pipeline/frangi_filter.py
@@ -3,10 +3,36 @@ import tifffile
 from src.io.im_info import ImInfo
 from src import xp, is_gpu, ndi, filters, xp_bk
 from src.utils.base_logger import logger
-import numpy as np
 
 
 class FrangiFilter:
+    """
+    Class for applying Frangi filter on images.
+
+    Args:
+        im_info (ImInfo): An ImInfo object containing information about the image.
+        alpha (float): Frangi filter parameter to adjust the contribution of the vesselness function to the final result.
+        beta (float): Frangi filter parameter to adjust the contribution of the blobness function to the final result.
+        num_sigma (int): Number of sigmas to use for multi-scale filtering.
+        sigma_min_max (tuple): Tuple containing the minimum and maximum sigma values to use for multi-scale filtering.
+        gamma (float): Frangi filter parameter to adjust the sensitivity of the filter to deviations from a blob-like structure.
+        frobenius_thresh (float): Threshold value used to suppress noisy responses.
+
+    Attributes:
+        im_info (ImInfo): An ImInfo object containing information about the image.
+        im_memmap (numpy.memmap): A memory-mapped numpy array of the image.
+        im_frangi (numpy.ndarray): An empty numpy array that will store the filtered image.
+        chunk_size (int): Number of slices to process at a time.
+        alpha (float): Frangi filter parameter to adjust the contribution of the vesselness function to the final result.
+        beta (float): Frangi filter parameter to adjust the contribution of the blobness function to the final result.
+        gamma (float): Frangi filter parameter to adjust the sensitivity of the filter to deviations from a blob-like structure.
+        frobenius_thresh (float): Threshold value used to suppress noisy responses.
+        num_sigma (int): Number of sigmas to use for multi-scale filtering.
+        sigma_min (float): Minimum sigma value to use for multi-scale filtering.
+        sigma_max (float): Maximum sigma value to use for multi-scale filtering.
+        sigmas (list): List of sigma values to use for multi-scale filtering.
+
+    """
     def __init__(
             self, im_info: ImInfo,
             alpha: float = 0.5, beta: float = 0.5,
@@ -15,12 +41,25 @@ class FrangiFilter:
             gamma: float = None,
             frobenius_thresh: float = None,
     ):
+        """
+        Constructor method for FrangiFilter class.
+
+        Args:
+            im_info (ImInfo): An ImInfo object containing information about the image.
+            alpha (float): Frangi filter parameter to adjust the contribution of the vesselness function to the final result.
+            beta (float): Frangi filter parameter to adjust the contribution of the blobness function to the final result.
+            num_sigma (int): Number of sigmas to use for multi-scale filtering.
+            sigma_min_max (tuple): Tuple containing the minimum and maximum sigma values to use for multi-scale filtering.
+            gamma (float): Frangi filter parameter to adjust the sensitivity of the filter to deviations from a blob-like structure.
+            frobenius_thresh (float): Threshold value used to suppress noisy responses.
+
+        Returns:
+            None
+
+        """
         self.im_info = im_info
         self.im_memmap = im_info.get_im_memmap(self.im_info.im_path)
         self.im_frangi = None
-        self.in_mem_1 = None
-        self.in_mem_2 = None
-        self.in_mem_z = None
         self.chunk_size = 20
         self.alpha = alpha
         self.beta = beta
@@ -28,20 +67,39 @@ class FrangiFilter:
         self.frobenius_thresh = frobenius_thresh
         self.num_sigma = num_sigma
         self.sigma_min, self.sigma_max = sigma_min_max
+
+        # If sigma_min_max is not specified, set default values based on image dimensions
         if (self.sigma_min is None) or (self.sigma_max is None):
             self._set_default_sigmas()
+
+        # Calculate the sigma step size
         min_sigma_step_size = 0.2
         sigma_step_size_calculated = (self.sigma_max - self.sigma_min) / self.num_sigma
         sigma_step_size = max(min_sigma_step_size, sigma_step_size_calculated)  # Avoid taking too small of steps.
         logger.debug(f'Calculated sigma step size = {sigma_step_size_calculated}. Using {sigma_step_size}')
+
+        # Create a list of sigma values to use for filtering
         self.sigmas = list(xp.arange(self.sigma_min, self.sigma_max, sigma_step_size))
 
     def _set_default_sigmas(self):
+        """
+        If sigma_min and sigma_max are not provided, sets default values for them based on the dimensions of the input image.
+        """
         logger.debug('No sigma values provided, setting to defaults.')
         self.sigma_min = self.im_info.dim_sizes['X'] * 10
         self.sigma_max = self.im_info.dim_sizes['X'] * 15
 
     def _gaussian_filter(self, sigma, t_num):
+        """
+        Applies a Gaussian filter to a specified slice of the input image at a given time step.
+
+        Args:
+            sigma (float): Standard deviation for Gaussian kernel.
+            t_num (int): Index for time step.
+
+        Returns:
+            numpy.ndarray: Filtered slice of the input image.
+        """
         if len(self.im_memmap[t_num, ...].shape) == 3:
             z_ratio = self.im_info.dim_sizes['Z'] / self.im_info.dim_sizes['X']
             sigma_vec = (sigma / z_ratio, sigma, sigma)
@@ -57,6 +115,15 @@ class FrangiFilter:
         return gauss_volume
 
     def _find_gamma(self, gauss_volume):
+        """
+        Determines gamma value to be used in Frangi filtering.
+
+        Args:
+            gauss_volume (numpy.ndarray): Filtered slice of the input image.
+
+        Returns:
+            float: Gamma value.
+        """
         if self.gamma is None:
             gamma_tri = filters.threshold_triangle(gauss_volume[gauss_volume > 0])
             gamma_otsu = filters.threshold_otsu(gauss_volume[gauss_volume > 0])
@@ -69,6 +136,9 @@ class FrangiFilter:
         return gamma
 
     def _get_h(self, gauss_slice):
+        """
+        Calculate the Hessian matrix of a 3D Gaussian slice using the second order partial derivatives.
+        """
         dz, dy, dx = xp.gradient(gauss_slice)
         dzz, _, _ = xp.gradient(dz)
         dyz, dyy, _ = xp.gradient(dy)
@@ -79,9 +149,19 @@ class FrangiFilter:
         return h_matrix
 
     def _get_h_mask(self, h_matrix):
+        """
+        Compute a mask for the Hessian matrix based on the Frobenius norm of its elements.
+
+        Args:
+            h_matrix: 3x3 array of the Hessian matrix
+
+        Returns:
+            bool: Mask of elements of Hessian matrix where Frobenius norm is greater than the threshold value
+        """
         frobenius_norm = xp.linalg.norm(
             xp.concatenate([h_matrix[0], h_matrix[1], h_matrix[2]], axis=0),
             axis=0)
+        # replace inf values with max finite value
         frobenius_norm[xp.isinf(frobenius_norm)] = xp.nanmax(frobenius_norm[~xp.isinf(frobenius_norm)])
         if self.frobenius_thresh is None:
             frobenius_threshold = xp.sqrt(xp.nanmax(frobenius_norm))
@@ -91,6 +171,16 @@ class FrangiFilter:
         return mask
 
     def _get_filtered_im(self, h_matrix, gamma):
+        """
+        Calculate the filtered image for the given Hessian matrix and gamma value.
+
+        Args:
+            h_matrix: 3x3 array of the Hessian matrix
+            gamma: gamma value for the filter
+
+        Returns:
+            tuple: filtered image and sorted eigenvalues
+        """
         alpha_sq = 2 * self.alpha ** 2
         beta_sq = 2 * self.beta ** 2
         gamma_sq = 2 * gamma ** 2
@@ -110,11 +200,30 @@ class FrangiFilter:
         return filtered_im, eig_sort
 
     def _mask_h_matrix(self, h_matrix):
+        """
+        Masks the Hessian matrix based on a threshold defined by the Frobenius norm.
+
+        Parameters:
+            h_matrix (numpy.ndarray): 3x3 Hessian matrix.
+
+        Returns:
+            tuple: A masked Hessian matrix and a boolean mask indicating which elements of the original matrix were kept.
+        """
         h_mask = self._get_h_mask(h_matrix)
         h_matrix = xp.transpose(h_matrix[:, :, h_mask], (2, 1, 0))
         return h_matrix, h_mask
 
     def _filter_with_sigma(self, sigma, t_num):
+        """
+        Applies the Frangi filter to a volume of images convolved with a Gaussian kernel.
+
+        Parameters:
+            sigma (float): Standard deviation of the Gaussian kernel.
+            t_num (int): Number of timepoints in the image stack.
+
+        Returns:
+            numpy.ndarray: Filtered image stack.
+        """
         gauss_volume = self._gaussian_filter(sigma, t_num)
         gamma = self._find_gamma(gauss_volume)
         num_z = gauss_volume.shape[0]
@@ -148,6 +257,12 @@ class FrangiFilter:
         return filtered_volume
 
     def run_filter(self, num_t: int = None):
+        """
+        Runs the Frangi filter on the input image stack and saves the filtered stack as a memory-mapped file.
+
+        Parameters:
+            num_t (int): Number of timepoints to filter. If not specified, all timepoints are filtered.
+        """
         logger.info('Allocating memory for frangi filtered image.')
         im_path = self.im_info.path_im_frangi
         if num_t is not None:
