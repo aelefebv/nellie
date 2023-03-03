f77d257370be350d2ca8e132bc45f38b5ef71384
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-03-03 12:33:35 -0800

    Fix organelle props to relabel skeleton image and give unscaled coords

--- src/pipeline/organelle_props.py
+++ src/pipeline/organelle_props.py
@@ -1,15 +1,14 @@
 from src.io.im_info import ImInfo
-from src import xp, ndi, measure, is_gpu
+from src import xp, measure, logger
 import tifffile
 
 
 class OrganelleProperties:
-    def __init__(self, label_region, skel_region):
-        self.instance_label = label_region.label
-        self.centroid = label_region.centroid
-        self.coords = label_region.coords
-        self.skeleton_coords = skel_region.coords
-
+    def __init__(self, organelle, skel_coords):
+        self.instance_label = organelle.label
+        self.centroid = organelle.centroid
+        self.coords = organelle.coords
+        self.skeleton_coords = skel_coords
 
 
 class OrganellePropertiesConstructor:
@@ -22,40 +21,41 @@ class OrganellePropertiesConstructor:
         self.organelles = []
 
     def measure_organelles(self, num_t: int = None):
-        # could potentially include intensity-weighted centroid, but not sure it's necessary
+        # could potentially include intensity-weighted centroid, but not sure that it's necessary
         label_im = tifffile.memmap(self.im_info.path_im_label_obj, mode='r')
-        skel_im = tifffile.memmap(self.im_info.path_im_mask, mode='r+')
+        skel_im = tifffile.memmap(self.im_info.path_im_skeleton, mode='r+')
 
         # Load only a subset of frames if num_t is not None
         if num_t is not None:
-            num_t = min(num_t, semantic_mask.shape[0])
+            num_t = min(num_t, label_im.shape[0])
             label_im = label_im[:num_t, ...]
             skel_im = skel_im[:num_t, ...]
 
         for frame_num, frame in enumerate(label_im):
+            logger.info(f'Getting organelle properties, volume {frame_num}/{len(label_im)}')
             label_frame = xp.asarray(label_im[frame_num])
             skel_frame = xp.asarray(skel_im[frame_num])
-            skel_label_set = set(skel_frame)  # get all the unique labels
 
-            label_props = measure.regionprops(label_im, spacing=self.spacing)
+            label_props = measure.regionprops(label_frame, spacing=self.spacing)
+            label_dict = {props.label: props for props in label_props}
             skel_props = measure.regionprops(skel_frame, spacing=self.spacing)
+            skel_dict = {props.label: props for props in skel_props}
 
             organelles_frame = []
-            for organelle in label_props:
+            for label_num, label_prop in label_dict.items():
 
                 # For some reason, skeletonization misses some small ones. This assigns centroid as skeleton.
-                if label_region.label not in skel_label_set:
-                    skel_region = None
-                    # todo need to make sure spacing still allows setting proper coordinates.
-                    skel_region.coords = label_region.centroid
+                if label_num not in skel_dict.keys():
+                    # current gpu implementation does not give unscaled centroid, so I rederive them here
+                    unscaled_centroid = [round(label_prop.centroid[i] / self.spacing[i])
+                                         for i in range(len(self.spacing))]
+                    unscaled_centroid.insert(0, frame_num)
+                    skel_im[tuple(unscaled_centroid)] = label_num
+                    skel_coords = label_prop.centroid
+                else:
+                    skel_coords = skel_dict[label_num].coords
 
-                    # labels skeleton image as well.
-                    if is_gpu:
-                        skel_im[skel_region.coords] = region.label.get()
-                    else:
-                        skel_im[skel_region.coords] = region.label
-
-                organelles_frame.append(OrganelleProperties(label_region, skel_region))
+                organelles_frame.append(OrganelleProperties(label_prop, skel_coords))
 
             self.organelles.append(organelles_frame)
 
