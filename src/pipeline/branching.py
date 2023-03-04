3eb00ddbadd65c27d685cc0e4c845df0393a4a0d
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2023-03-04 00:38:22 -0600

    Reconnect branches that have smallest joining angle

--- src/pipeline/branching.py
+++ src/pipeline/branching.py
@@ -1,4 +1,4 @@
-from src import xp, ndi, logger, is_gpu
+from src import xp, ndi, logger
 from src.io.im_info import ImInfo
 import tifffile
 
@@ -27,4 +27,43 @@ class BranchSegments:
 
         # Label any individual branch and save it to the memmap.
         for frame_num, frame in enumerate(neighbor_im):
-            logger.info(f'Running neighborhood analysis, volume {frame_num}/{len(neighbor_im)}')
+            logger.info(f'Running branch point analysis, volume {frame_num}/{len(neighbor_im)}')
+            # segment individual branches
+            edge_points = frame == 2
+            branch_labels, _ = ndi.label(edge_points)
+
+            # loop over each branch point
+            branch_idx = xp.argwhere(frame == 3)
+            edge_idx = xp.argwhere(frame == 2)
+            for branch_num, bp_idx in enumerate(branch_idx):
+                logger.debug(f'Reconnection branch {branch_num}/{len(branch_idx)}')
+
+                # initialize variables to keep track of the smallest angle and corresponding branch labels
+                min_angle = xp.inf
+                connect_label_1 = None
+                connect_label_2 = None
+
+                # find the neighboring branch points connected to the current branch point
+                neigh_idx = xp.array([idx for idx in edge_idx if xp.abs(idx - bp_idx).sum() == 1])
+
+                # loop over each pair of neighboring branch points
+                for i in range(len(neigh_idx) - 1):
+                    for j in range(i + 1, len(neigh_idx)):
+                        # compute the angle between the two neighboring branch points
+                        segment_1 = neigh_idx[i] - bp_idx
+                        segment_2 = neigh_idx[j] - bp_idx
+
+                        # angle calculation using dot product formula for the angle between 2 vectors
+                        angle = xp.abs(xp.arccos(
+                            xp.dot(segment_1, segment_2) / (xp.linalg.norm(segment_1) * xp.linalg.norm(segment_2))))
+                        # * 180 / xp.pi  # don't need this multiplier since doing comparisons
+
+                        # if the angle between the two branches is smaller than the current smallest angle,
+                        # update the smallest angle and the branch indices
+                        if angle < min_angle:
+                            min_angle = angle
+                            connect_label_1 = branch_labels[neigh_idx[i]]
+                            connect_label_2 = branch_labels[neigh_idx[j]]
+
+                # relabel label 2 to label 1
+                self.segment_memmap[frame][self.segment_memmap[frame] == connect_label_2] = connect_label_1
