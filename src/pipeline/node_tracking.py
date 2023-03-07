eece44b77cc3bec174d350eebf96edce479bffbe
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2023-03-07 11:43:35 -0800

    Assignment matrix incorporates time gaps and optional time and distance thresholds

--- src/pipeline/node_tracking.py
+++ src/pipeline/node_tracking.py
@@ -23,43 +23,57 @@ class NodeTrack:
 
 
 class NodeTrackConstructor:
-    def __init__(self, im_info: ImInfo, distance_thresh_um: float = xp.inf, time_thresh_s: float = xp.inf):
+    def __init__(self, im_info: ImInfo,
+                 distance_thresh_um_per_sec: float = xp.inf,
+                 time_thresh_sec: float = xp.inf):
         self.im_info = im_info
         self.node_constructor: NodeConstructor = unpickle_object(self.im_info.path_pickle_node)
         self.nodes: [[Node]] = self.node_constructor.nodes
         self.tracks: [NodeTrack] = []
         self.num_frames = len(self.nodes)
-        self.distance_thresh_um = distance_thresh_um
-        self.time_thresh_s = time_thresh_s
+        self.distance_thresh_um_per_sec = distance_thresh_um_per_sec
+        self.time_thresh_sec = time_thresh_sec
 
     def initialize_tracks(self):
         for node in self.nodes[0]:
             self.tracks.append(NodeTrack(node))
 
-    def assign_nodes_to_tracks(self, frame_num):
+    def get_assignment_matrix(self, frame_num):
         frame_nodes = self.nodes[frame_num]
         num_nodes = len(frame_nodes)
         num_tracks = len(self.tracks)
         num_dimensions = len(frame_nodes[0].centroid_um)
+        frame_time_s = frame_nodes[0].time_point_sec
 
         node_centroids = xp.empty((num_dimensions, 1, num_nodes))
         track_centroids = xp.empty((num_dimensions, num_tracks, 1))
 
+        time_matrix = xp.empty((num_tracks, num_nodes))
+
         for node_num, node in enumerate(frame_nodes):
             node_centroids[:, 0, node_num] = node.centroid_um
         for track_num, track in enumerate(self.tracks):
+            time_check = frame_time_s - track.time_points[-1]
+            if time_check > self.time_thresh_sec:
+                track_centroids[:, track_num, 0] = xp.nan
+                continue
             track_centroids[:, track_num, 0] = track.centroids_um[-1]
+            time_matrix[track_num, :] = time_check
 
         distance_matrix = xp.sqrt(xp.sum((node_centroids - track_centroids) ** 2, axis=0))
-        distance_matrix[distance_matrix > self.distance_thresh_um] = xp.inf
-        # todo do the same for time
+        distance_matrix /= time_matrix  # this is now a distance/sec matrix
+        distance_matrix[distance_matrix > self.distance_thresh_um_per_sec] = xp.nan
+
+        return distance_matrix
 
     def populate_tracks(self, num_t: int = None):
         if num_t is not None:
             num_t = min(num_t, self.num_frames)
+            self.num_frames = num_t
         for frame_num in range(self.num_frames):
-            test_dist = self.assign_nodes_to_tracks(frame_num)
-        return test_dist
+            assignment_matrix = self.get_assignment_matrix(frame_num)
+
+        return assignment_matrix
 
 
 if __name__ == "__main__":
@@ -72,6 +86,6 @@ if __name__ == "__main__":
     except FileNotFoundError:
         logger.error("File not found.")
         exit(1)
-    nodes_test = NodeTrackConstructor(test)
+    nodes_test = NodeTrackConstructor(test, distance_thresh_um_per_sec=0.5)
     nodes_test.initialize_tracks()
     dist_matrix = nodes_test.populate_tracks()
