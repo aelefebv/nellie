1bc2f7170d51fd6a3eb0f1f87552b85947ab5308
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-10-04 18:08:46 -0700

    Incorporate local intensity to determine if object should be kept

--- src_2/segmentation/labelling.py
+++ src_2/segmentation/labelling.py
@@ -17,39 +17,43 @@ class Label:
         self.min_radius_um = max(min_radius_um, self.im_info.dim_sizes['X'])
         self.max_radius_um = max_radius_um
 
+        self.im_memmap = None
         self.frangi_memmap = None
+        # remove objects less than 9 voxels
+        self.min_size_threshold_px = 9
+        self.max_size_threshold_px = xp.inf
 
         self.remove_in_2d = False
-        if any(xp.array(
-                [self.im_info.dim_sizes['Z'], self.im_info.dim_sizes['Y'], self.im_info.dim_sizes['X']]
-        ) > (self.min_radius_um*2)):
-            logger.warning(f"One of the dimensions' voxel sizes is greater than the minimum radius of the structure in "
-                           f"question so object removal will be conducted based on 2D parameters instead of 3D. "
-                           f"This may result in objects being kept that should not be.")
-            self.remove_in_2d = True
-
-        # convert min radius um to a min area / volume
-        if not self.im_info.no_z and not self.remove_in_2d:
-            # volume of sphere of radius min_width/2 in pixels cubed
-            self.min_size_threshold_px = (4 / 3 * xp.pi * (min_radius_um / 2) ** 2) / (
-                    self.im_info.dim_sizes['X'] ** 2 * self.im_info.dim_sizes['Z']
-            )
-            # min area of 3*3*3 pixels
-            self.min_size_threshold_px = max(self.min_size_threshold_px, 27)
-            self.max_size_threshold_px = (4 / 3 * xp.pi * (max_radius_um / 2) ** 2) / (
-                    self.im_info.dim_sizes['X'] ** 2 * self.im_info.dim_sizes['Z']
-            )
-        else:
-            self.min_size_threshold_px = (xp.pi * (min_radius_um / 2) ** 2) / (self.im_info.dim_sizes['X'] ** 2)
-            # min area of 3*3 pixels
-            self.min_size_threshold_px = max(self.min_size_threshold_px, 9)
-            self.max_size_threshold_px = (xp.pi * (max_radius_um / 2) ** 2) / (self.im_info.dim_sizes['X'] ** 2)
+        # if any(xp.array(
+        #         [self.im_info.dim_sizes['Z'], self.im_info.dim_sizes['Y'], self.im_info.dim_sizes['X']]
+        # ) > (self.min_radius_um*2)):
+        #     logger.warning(f"One of the dimensions' voxel sizes is greater than the minimum radius of the structure in "
+        #                    f"question so object removal will be conducted based on 2D parameters instead of 3D. "
+        #                    f"This may result in objects being kept that should not be.")
+        #     self.remove_in_2d = True
+        #
+        # # convert min radius um to a min area / volume
+        # if not self.im_info.no_z and not self.remove_in_2d:
+        #     # volume of sphere of radius min_width/2 in pixels cubed
+        #     self.min_size_threshold_px = (4 / 3 * xp.pi * (min_radius_um / 2) ** 2) / (
+        #             self.im_info.dim_sizes['X'] ** 2 * self.im_info.dim_sizes['Z']
+        #     )
+        #     # min area of 3*3*3 pixels
+        #     self.min_size_threshold_px = max(self.min_size_threshold_px, 27)
+        #     self.max_size_threshold_px = (4 / 3 * xp.pi * (max_radius_um / 2) ** 2) / (
+        #             self.im_info.dim_sizes['X'] ** 2 * self.im_info.dim_sizes['Z']
+        #     )
+        # else:
+        #     self.min_size_threshold_px = (xp.pi * (min_radius_um / 2) ** 2) / (self.im_info.dim_sizes['X'] ** 2)
+        #     # min area of 3*3 pixels
+        #     self.min_size_threshold_px = max(self.min_size_threshold_px, 9)
+        #     self.max_size_threshold_px = (xp.pi * (max_radius_um / 2) ** 2) / (self.im_info.dim_sizes['X'] ** 2)
 
         self.semantic_mask_memmap = None
         self.instance_label_memmap = None
         self.shape = ()
 
-        self.debug = None
+        self.debug = {}
 
     def _get_t(self):
         if self.num_t is None:
@@ -62,6 +66,9 @@ class Label:
 
     def _allocate_memory(self):
         logger.debug('Allocating memory for semantic segmentation.')
+        im_memmap = self.im_info.get_im_memmap(self.im_info.im_path)
+        self.im_memmap = get_reshaped_image(im_memmap, self.num_t, self.im_info)
+
         frangi_memmap = self.im_info.get_im_memmap(self.im_info.pipeline_paths['im_frangi'])
         self.frangi_memmap = get_reshaped_image(frangi_memmap, self.num_t, self.im_info)
         self.shape = self.frangi_memmap.shape
@@ -76,7 +83,7 @@ class Label:
                                                                   description='instance segmentation',
                                                                   return_memmap=True)
 
-    def _remove_bad_sized_objects(self, frame):
+    def _get_labels(self, frame):
         ndim = 2 if self.remove_in_2d else 3
         footprint = ndi.generate_binary_structure(ndim, 1)
 
@@ -85,6 +92,17 @@ class Label:
         mask = ndi.binary_opening(mask, structure=xp.ones((2, 2, 2)))
 
         labels, _ = ndi.label(mask, structure=footprint)
+        return mask, labels
+
+    def _remove_bad_sized_objects(self, labels):
+        ndim = 2 if self.remove_in_2d else 3
+        footprint = ndi.generate_binary_structure(ndim, 1)
+        #
+        # mask = frame > 0
+        # mask = ndi.binary_fill_holes(mask)
+        # mask = ndi.binary_opening(mask, structure=xp.ones((2, 2, 2)))
+        #
+        # labels, _ = ndi.label(mask, structure=footprint)
         label_sizes = xp.bincount(labels.ravel())
 
         above_threshold = label_sizes > self.min_size_threshold_px
@@ -111,17 +129,65 @@ class Label:
     #     trimmed_labels, _ = ndi.label(trimmed_labels > 0)
     #     return trimmed_labels
 
+    def _get_subtraction_mask(self, original_frame, labels_frame):
+        subtraction_mask = original_frame.copy()
+        subtraction_mask[labels_frame > 0] = 0
+        return subtraction_mask
+
+    def _get_object_snrs(self, original_frame, labels_frame):
+        logger.debug('Calculating object SNRs.')
+        subtraction_mask = self._get_subtraction_mask(original_frame, labels_frame)
+        unique_labels = xp.unique(labels_frame)
+        extend_bbox_by = 1
+        keep_labels = []
+        for label in unique_labels:
+            if label == 0:
+                continue
+            coords = xp.nonzero(labels_frame == label)
+            z_coords, r_coords, c_coords = coords
+
+            zmin, zmax = xp.min(z_coords), xp.max(z_coords)
+            rmin, rmax = xp.min(r_coords), xp.max(r_coords)
+            cmin, cmax = xp.min(c_coords), xp.max(c_coords)
+
+            zmin, zmax = xp.clip(zmin - extend_bbox_by, 0, labels_frame.shape[0]), xp.clip(zmax + extend_bbox_by, 0,
+                                                                                           labels_frame.shape[0])
+            rmin, rmax = xp.clip(rmin - extend_bbox_by, 0, labels_frame.shape[1]), xp.clip(rmax + extend_bbox_by, 0,
+                                                                                           labels_frame.shape[1])
+            cmin, cmax = xp.clip(cmin - extend_bbox_by, 0, labels_frame.shape[2]), xp.clip(cmax + extend_bbox_by, 0,
+                                                                                           labels_frame.shape[2])
+
+            # only keep objects over 1 std from its surroundings
+            local_intensity = subtraction_mask[zmin:zmax, rmin:rmax, cmin:cmax]
+            local_intensity_mean = local_intensity[local_intensity > 0].mean()
+            local_intensity_std = local_intensity[local_intensity > 0].std()
+            label_intensity_mean = original_frame[coords].mean()
+            intensity_cutoff = label_intensity_mean / (local_intensity_mean + local_intensity_std)
+            if intensity_cutoff > 1:
+                keep_labels.append(label)
+
+        keep_labels = xp.asarray(keep_labels)
+        labels_frame = xp.where(xp.isin(labels_frame, keep_labels), labels_frame, 0)
+        return labels_frame
+
+
     def _run_frame(self, t):
         logger.info(f'Running semantic segmentation, volume {t}/{self.num_t - 1}')
-        frame_in_mem = xp.asarray(self.frangi_memmap[t, ...])
+        original_in_mem = xp.asarray(self.im_memmap[t, ...])
+        frangi_in_mem = xp.asarray(self.frangi_memmap[t, ...])
+        _, labels = self._get_labels(frangi_in_mem)
         # threshold_mask, threshold_labels = self._remove_bad_sized_objects(frame_in_mem)
         # trimmed_labels = self._trim_labels(frame_in_mem, threshold_labels)
-        return self._remove_bad_sized_objects(frame_in_mem)
+        _, cleaned_labels = self._remove_bad_sized_objects(labels)
+        cleaned_labels = self._get_object_snrs(original_in_mem, cleaned_labels)
+        return cleaned_labels
+        # return labels
 
     def _run_segmentation(self):
         for t in range(self.num_t):
-            _, instance_mask = self._run_frame(t)
-            self.instance_label_memmap[t, ...] = instance_mask.get()
+            labels = self._run_frame(t)
+            self.instance_label_memmap[t, ...] = labels.get()
+            break
 
     def run(self):
         logger.info('Running semantic segmentation.')
@@ -143,7 +209,7 @@ if __name__ == "__main__":
         im_infos.append(im_info)
 
     segmentations = []
-    for im_info in im_infos:
+    for im_info in im_infos[-1:]:
         segment_unique = Label(im_info, num_t=2)
         segment_unique.run()
         segmentations.append(segment_unique)
