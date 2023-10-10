dc21b748757735844879f64ab6309233ff08992e
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-10-10 12:02:00 -0700

    Hu moment calculation seems to be working

--- src_2/tracking/hu.py
+++ src_2/tracking/hu.py
@@ -24,27 +24,42 @@ class HuMomentTracking:
 
     def _calculate_normalized_moments(self, image):
         # raw moments
-        x, y = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0]))
+        x, y = xp.meshgrid(xp.arange(image.shape[1]), xp.arange(image.shape[0]))
         M = {}
-        for i in range(3):
-            for j in range(3):
-                M[str(i) + str(j)] = np.sum(x ** i * y ** j * image)
+        for i in range(4):
+            for j in range(4):
+                M[str(i) + str(j)] = xp.sum(x ** i * y ** j * image)
 
         # central moments
-        x, y = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0]))
+        x, y = xp.meshgrid(xp.arange(image.shape[1]), xp.arange(image.shape[0]))
         x_bar = M['10'] / M['00']
         y_bar = M['01'] / M['00']
         mu = {}
-        for i in range(3):
-            for j in range(3):
-                mu[str(i) + str(j)] = np.sum((x - x_bar) ** i * (y - y_bar) ** j * image)
+        for i in range(4):
+            for j in range(4):
+                mu[str(i) + str(j)] = xp.sum((x - x_bar) ** i * (y - y_bar) ** j * image)
 
         # normalized moments
         eta = {}
-        for i in range(3):
-            for j in range(3):
+        for i in range(4):
+            for j in range(4):
                 eta[str(i) + str(j)] = mu[str(i) + str(j)] / (M['00'] ** ((i + j + 2) / 2))
         return eta
+
+    def _calculate_hu_moments(self, eta):
+        eta_20, eta_02, eta_11 = eta['20'], eta['02'], eta['11']
+        eta_30, eta_12, eta_21, eta_03 = eta['30'], eta['12'], eta['21'], eta['03']
+
+        hu = np.zeros(7)
+        hu[0] = eta_20 + eta_02
+        hu[1] = (eta_20 - eta_02) ** 2 + 4 * eta_11 ** 2
+        hu[2] = (eta_30 - 3 * eta_12) ** 2 + (3 * eta_21 - eta_03) ** 2
+        hu[3] = (eta_30 + eta_12) ** 2 + (eta_21 + eta_03) ** 2
+        hu[4] = (eta_30 - 3 * eta_12) * (eta_30 + eta_12) * ((eta_30 + eta_12) ** 2 - 3 * (eta_21 + eta_03) ** 2) + \
+                (3 * eta_21 - eta_03) * (eta_21 + eta_03) * (3 * (eta_30 + eta_12) ** 2 - (eta_21 + eta_03) ** 2)
+        hu[5] = (eta_20 - eta_02) * ((eta_30 + eta_12) ** 2 - (eta_21 + eta_03) ** 2) + \
+                4 * eta_11 * (eta_30 + eta_12) * (eta_21 + eta_03)
+        return hu[:5]  # Return the first 5 Hu moments
 
     def _get_t(self):
         if self.num_t is None:
@@ -78,10 +93,38 @@ class HuMomentTracking:
         distance_frame = ndi.maximum_filter(xp.array(self.im_distance_memmap[t]), size=3)
         marker_frame = xp.array(self.im_marker_memmap[t]) > 0
         marker_indices = xp.argwhere(marker_frame)
-        all_radii = distance_frame[marker_frame]
+        # all_radii = distance_frame[marker_frame]
 
         test_marker = marker_indices[50]
-        test_marker_radius = distance_frame[test_marker[0], test_marker[1], test_marker[2]]
+        test_marker_radius = int(xp.ceil(distance_frame[test_marker[0], test_marker[1], test_marker[2]]))
+        # NxNxN region of intensity im
+        z_low = max(0, test_marker[0] - test_marker_radius)
+        z_high = min(self.shape[1], test_marker[0] + test_marker_radius)
+        y_low = max(0, test_marker[1] - test_marker_radius)
+        y_high = min(self.shape[2], test_marker[1] + test_marker_radius)
+        x_low = max(0, test_marker[2] - test_marker_radius)
+        x_high = min(self.shape[3], test_marker[2] + test_marker_radius)
+
+        test_region = intensity_frame[z_low:z_high, y_low:y_high, x_low:x_high]
+        z_proj = xp.max(test_region, axis=0)
+        y_proj = xp.max(test_region, axis=1)
+        x_proj = xp.max(test_region, axis=2)
+
+        z_moments = self._calculate_normalized_moments(z_proj)
+        y_moments = self._calculate_normalized_moments(y_proj)
+        x_moments = self._calculate_normalized_moments(x_proj)
+
+        z_hu = self._calculate_hu_moments(z_moments)
+        y_hu = self._calculate_hu_moments(y_moments)
+        x_hu = self._calculate_hu_moments(x_moments)
+
+        #rotate x 90 degress
+        x_rotated = xp.rot90(x_proj, k=0, axes=(0, 1))
+        x_rot_moments = self._calculate_normalized_moments(x_rotated)
+        x_rot_hu = self._calculate_hu_moments(x_rot_moments)
+        x_zoom2 = ndi.zoom(x_proj, zoom=2)
+        x_zoom2_moments = self._calculate_normalized_moments(x_zoom2)
+        x_zoom2_hu = self._calculate_hu_moments(x_zoom2_moments)
 
     def _run_pso(self):
         for t in range(self.num_t):
