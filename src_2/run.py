c86225b45f2768cc037b71fa3b25862e7332d8d5
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-12-12 12:26:59 -0800

    Fixed flipping on non-MPS systems

--- src_2/run.py
+++ src_2/run.py
@@ -43,16 +43,16 @@ def run(im_path, num_t=None, remove_edges=True, ch=0):
     return im_info
 
 if __name__ == "__main__":
-    # im_path = r"D:\test_files\stress_granules\single\deskewed-2023-04-13_17-34-08_000_AELxES-stress_granules-dmr_perk-activate_deactivate-1nM-activate.ome.tif"
-    # im_info = run(im_path, remove_edges=True, ch=1, num_t=3)
-    import os
-    # top_dir = r"D:\test_files\stress_granules"
-    top_dir = r"D:\test_files\nelly_multichannel"
-    # get all non-folder files
-    all_files = os.listdir(top_dir)
-    all_files = [os.path.join(top_dir, file) for file in all_files if not os.path.isdir(os.path.join(top_dir, file))]
-    # all_files = [r"D:\test_files\nelly_tests\deskewed-2023-07-13_14-58-28_000_wt_0_acquire.ome.tif"]
-    for file_num, tif_file in enumerate(all_files):
-        for ch in range(2):
-            print(f'Processing file {file_num + 1} of {len(all_files)}, channel {ch + 1} of 2')
-            im_info = run(tif_file, remove_edges=True, num_t=3, ch=ch)
+    im_path = r"D:\test_files\stress_granules\single\deskewed-2023-04-13_17-34-08_000_AELxES-stress_granules-dmr_perk-activate_deactivate-1nM-activate.ome.tif"
+    im_info = run(im_path, remove_edges=True, ch=1, num_t=3)
+    # import os
+    # # top_dir = r"D:\test_files\stress_granules"
+    # top_dir = r"D:\test_files\nelly_multichannel"
+    # # get all non-folder files
+    # all_files = os.listdir(top_dir)
+    # all_files = [os.path.join(top_dir, file) for file in all_files if not os.path.isdir(os.path.join(top_dir, file))]
+    # # all_files = [r"D:\test_files\nelly_tests\deskewed-2023-07-13_14-58-28_000_wt_0_acquire.ome.tif"]
+    # for file_num, tif_file in enumerate(all_files):
+    #     for ch in range(2):
+    #         print(f'Processing file {file_num + 1} of {len(all_files)}, channel {ch + 1} of 2')
+    #         im_info = run(tif_file, remove_edges=True, num_t=3, ch=ch)
--- src_2/utils/gpu_functions.py
+++ src_2/utils/gpu_functions.py
@@ -1,4 +1,4 @@
-from src import xp
+from src import xp, device_type
 
 
 def otsu_effectiveness(image, inter_variance):
@@ -16,12 +16,16 @@ def otsu_threshold(matrix, nbins=256):
     counts = counts / xp.sum(counts)
 
     weight1 = xp.cumsum(counts)
-    weight2 = xp.cumsum(xp.flip(counts, dims=[0]))
-    weight2 = xp.flip(weight2, dims=[0])
     mean1 = xp.cumsum(counts * bin_centers) / weight1
-    flipped_counts_bin_centers = xp.flip(counts * bin_centers, dims=[0])
-    cumsum_flipped = xp.cumsum(flipped_counts_bin_centers)
-    mean2 = xp.flip(cumsum_flipped / xp.flip(weight2, dims=[0]), dims=[0])
+    if device_type == 'mps':
+        weight2 = xp.cumsum(xp.flip(counts, dims=[0]))
+        weight2 = xp.flip(weight2, dims=[0])
+        flipped_counts_bin_centers = xp.flip(counts * bin_centers, dims=[0])
+        cumsum_flipped = xp.cumsum(flipped_counts_bin_centers)
+        mean2 = xp.flip(cumsum_flipped / xp.flip(weight2, dims=[0]), dims=[0])
+    else:
+        weight2 = xp.cumsum(counts[::-1])[::-1]
+        mean2 = (xp.cumsum((counts * bin_centers)[::-1]) / weight2[::-1])[::-1]
 
     variance12 = weight1[:-1] * weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
 
@@ -42,7 +46,8 @@ def triangle_threshold(matrix, nbins=256):
 
     flip = arg_peak_height - arg_low_level < arg_high_level - arg_peak_height
     if flip:
-        hist = xp.flip(hist, dims=[0])
+        if device_type == 'mps':
+            hist = xp.flip(hist, dims=[0])
         arg_low_level = nbins - arg_high_level - 1
         arg_peak_height = nbins - arg_peak_height - 1
     del(arg_high_level)
