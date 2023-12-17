1baad683411994fa6b0e055f97005620280a4b51
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-12-16 17:43:54 -0800

    Added a few more explorations

--- src/analysis/multimesh_GNN/compare_multifiles.py
+++ src/analysis/multimesh_GNN/compare_multifiles.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 import datetime
 from scipy.optimize import curve_fit
+from scipy.fft import fft
+
 
 # YYYYMMDD_HHMMSS
 current_dt = datetime.datetime.now()
@@ -63,10 +65,24 @@ def get_recovery(x, y):
     r_squared = 1 - (ss_res / ss_tot)
     return r_squared, fit_params
 
-def plot_embedding_changes(dissimilarity_to_control, save_plots=False, save_path=None):
+def plot_similarity_matrix(similarity_matrix, save_plots=False, save_path=None):
+    plt.figure(figsize=(8, 6))
+    plt.imshow(similarity_matrix, cmap='turbo')
+    plt.xlabel('File Number')
+    plt.ylabel('File Number')
+    plt.title('Similarity Matrix of All Dataset Embeddings')
+    if save_plots:
+        plt.savefig(os.path.join(save_path, f'{current_dt_str}-similarity_matrix.png'))
+        plt.close()
+    else:
+        plt.show()
+
+def plot_embedding_changes(dissimilarity_to_control, save_plots=False, save_path=None, line=False):
     # plot similarity to control
     plt.figure(figsize=(8, 6))
-    plt.scatter(np.arange(len(dissimilarity_to_control)), dissimilarity_to_control, label='Similarity to Control')
+    if line:
+        plt.plot(np.arange(len(dissimilarity_to_control)), dissimilarity_to_control, 'r-', label='Similarity to Control')
+    plt.scatter(np.arange(len(dissimilarity_to_control)), dissimilarity_to_control)
     plt.xlabel('File Number')
     plt.ylabel('Dissimilarity to Control')
     plt.title('Dissimilarity of Embeddings to Control')
@@ -94,10 +110,11 @@ def plot_recovery(xdata, recovery_array, fit_params, r_squared, save_plots=False
     else:
         plt.show()
 
-def plot_tsne(reduced_mean_embeddings, labels, save_plots=False, save_path=None):
+def plot_tsne(reduced_mean_embeddings, labels, alpha=1, size=10, cmap='turbp', save_plots=False, save_path=None):
     # color is categorical by filename
     plt.figure(figsize=(8, 6))
-    plt.scatter(reduced_mean_embeddings[:, 0], reduced_mean_embeddings[:, 1], c=labels, cmap='turbo')
+    plt.scatter(reduced_mean_embeddings[:, 0], reduced_mean_embeddings[:, 1], c=labels, cmap=cmap,
+                alpha=alpha, s=size)
     plt.xlabel('t-SNE Feature 1')
     plt.ylabel('t-SNE Feature 2')
     plt.title('t-SNE Visualization of All Dataset Embeddings')
@@ -107,106 +124,105 @@ def plot_tsne(reduced_mean_embeddings, labels, save_plots=False, save_path=None)
     else:
         plt.show()
 
+def get_frequency_stats(vec):
+    fs = 1  # in Hz
+    duration = len(vec)  # in seconds
+    t = np.arange(0, duration, 1 / fs)
+
+    signal = vec - np.mean(vec)  # remove DC component
+
+    signal_fft = fft(signal)
+    freq = np.fft.fftfreq(len(t), 1 / fs)
+
+    # Compute the magnitude of the FFT (two-sided spectrum)
+    magnitude = np.abs(signal_fft)
+    # get all peaks, ignore negative peaks
+    magnitude = magnitude[:len(magnitude) // 2]
+    freq = freq[:len(freq) // 2]
+    # find all peaks
+    peak_indices = np.argsort(magnitude)[-5:][::-1]
+    peak_freqs = freq[peak_indices]
+    peak_magnitudes = magnitude[peak_indices]
+    print(f"The 5 strongest oscillating frequencies are: {peak_freqs}")
+    print(f"Their magnitudes are: {peak_magnitudes}")
+
 
 if __name__ == '__main__':
     model_path = r"D:\test_files\nelly_tests\20231215_145237-autoencoder - Copy.pt"
-
     dataset_paths = [
         r"D:\test_files\nelly_iono\deskewed-pre_0-19.ome.tif",
         r"D:\test_files\nelly_iono\full_2\deskewed-full_post_1.ome.tif",
     ]
 
+    # Import datasets and get some embeddings
     datasets, labels = import_datasets(dataset_paths)
     embeddings = get_embeddings(datasets, model_path)
-    avg_embeddings = [np.median(embed, axis=0) for embed in embeddings]
+    median_embeddings = [np.median(embed, axis=0) for embed in embeddings]
+    mean_embeddings = [np.mean(embed, axis=0) for embed in embeddings]
 
+    # All of our controls are from frames 0 to 18, so lets get the mean of those to compare to.
     control_timepoints = 18
-    mean_control_embeddings = np.mean(avg_embeddings[:control_timepoints], axis=0).tolist()
+    mean_control_embeddings = np.mean(median_embeddings[:control_timepoints], axis=0).tolist()
 
-    # mean_control and all treated
-    new_embeddings = np.array([mean_control_embeddings] + avg_embeddings[control_timepoints:])
+    # Let's have our controls as one embedding, and compare the rest to that
+    new_embeddings = np.array([mean_control_embeddings] + median_embeddings[control_timepoints:])
     new_labels = np.array([0] + labels[control_timepoints:])
     similarity_matrix = get_similarity_matrix(new_embeddings)
 
+    # Let's analyze how things change over time
     peak_dissimilarity = get_peak_difference_time(similarity_matrix)
-
     dissimilarity_to_control = 1-similarity_matrix[0, 1:]
     plot_embedding_changes(dissimilarity_to_control)
+    print(f"Peak dissimilarity is at frame {peak_dissimilarity}")
 
+    # Let's fit an exponential decay to the recovery curve to analyze recovery time
     recovery_array = dissimilarity_to_control[peak_dissimilarity:]
     xdata = np.arange(len(recovery_array))
     r_squared, fit_params = get_recovery(xdata, recovery_array)
     plot_recovery(xdata, recovery_array, fit_params, r_squared)
+    print(f"Recovery time is {1/fit_params[0][1]:.2f} frames")
 
-    stacked_embeddings = np.vstack(avg_embeddings)
-
+    # Let's plot all of our embeddings in 2D space to visualize any other trends
+    stacked_embeddings = np.vstack(median_embeddings)
     tsne = TSNE(n_components=2, random_state=42)
     reduced_embeddings = tsne.fit_transform(stacked_embeddings)
     frame_array = np.arange(len(stacked_embeddings))
     plot_tsne(reduced_embeddings, frame_array)
 
+    # Are there any other smaller patterns happening?
+    similarity_matrix = get_similarity_matrix(mean_embeddings)
+    # similarity_matrix = get_similarity_matrix(mean_embeddings[36:])
+    plot_similarity_matrix(similarity_matrix)
 
+    # Let's look at the frequency of the whole treatment curve
+    similarity_1 = similarity_matrix[0, 1:]
+    # low_pass filter
+    from scipy.signal import butter, filtfilt
 
-save_plots = False
-save_path = r"D:\test_files\nelly_tests"
+    # Let's get a filter for every 5 frames
+    b, a = butter(2, 0.2)
+    vec_filtered = filtfilt(b, a, similarity_1)
+    # Let's get a filter for every 20 frames to get a good average
+    b2, a2 = butter(2, 0.05)
+    vec_bigger_filter = filtfilt(b2, a2, similarity_1)
+    # Let's remove the low frequency component (remove the average)
+    low_freq_removed_and_filtered = vec_filtered - vec_bigger_filter
+    # Let's see what's happening after that.
+    plot_embedding_changes(low_freq_removed_and_filtered, line=True)
+    get_frequency_stats(low_freq_removed_and_filtered)
 
-ratio = similarity_to_least_similar / similarity_to_first
-ratio_test = ratio[18:] #ignore pre-treated
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.fft import fft
-
-# Sample rate and duration
-fs = 1  # in Hz
-duration = 97  # in seconds
-
-# Time array
-t = np.arange(0, duration, 1/fs)
-
-# Generate a signal, for example, a sine wave
-frequency = 50  # in Hz
-signal = ratio_test_filtered
-
-signal = signal - np.mean(signal)
-
-# Apply FFT
-signal_fft = fft(signal)
-
-# Compute the frequency axis
-freq = np.fft.fftfreq(len(t), 1/fs)
-
-# Compute the magnitude of the FFT (two-sided spectrum)
-magnitude = np.abs(signal_fft)
-
-# Find the first peak frequency
-magnitude[0] = 0  # ignore DC component
-first_peak_index = np.argmax(magnitude)
-first_peak_freq = freq[first_peak_index]
-
-# Zero out the first peak
-magnitude[first_peak_index] = 0
-
-# Find the second peak frequency
-second_peak_index = np.argmax(magnitude)
-second_peak_freq = freq[second_peak_index]
-
-
-# Plotting
-plt.plot(freq, magnitude)
-plt.title('FFT of the signal')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude')
-plt.xlim([0, fs/2])  # Limit x-axis to half the sampling rate
-plt.show()
-
-print(f"The 2nd strongest oscillating frequency is: {second_peak_freq:.2f} Hz")
-
-
-
-# ratio = similarity_to_first
-# low pass filter using scipy
-from scipy.signal import butter, filtfilt
-
-b, a = butter(2, 0.3)
-ratio_test_filtered = filtfilt(b, a, ratio_test)
+    # now let's find the individual nodes that are most dissimilar to the control
+    # eg for max timepoint after treatment:
+    max_timepoint = peak_dissimilarity + control_timepoints
+    node_diffs = np.abs(embeddings[max_timepoint] - mean_control_embeddings)
+    tsne = TSNE(n_components=2, random_state=42, perplexity=200)
+    max_timepoint_embeddings = embeddings[max_timepoint][::10]
+    last_timepoint_embeddings = embeddings[-1][::10]
+    # combined
+    reduced_embeddings = tsne.fit_transform(np.vstack((max_timepoint_embeddings, last_timepoint_embeddings)))
+    labels = np.array([0] * len(max_timepoint_embeddings) + [1] * len(last_timepoint_embeddings))
+    # color array by mean node difference value
+    mean_node_diffs = np.mean(node_diffs, axis=1)
+    plot_tsne(reduced_embeddings, mean_node_diffs[::10], alpha=0.5, size=2)
+    plot_tsne(reduced_embeddings, labels, alpha=0.75, size=5, cmap='bwr')
 
