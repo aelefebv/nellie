36692e3bd7c001ae3b66084d54d2f9fe7a01ffbb
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2023-12-19 10:26:23 -0800

    Added more comments for multiorganelle decon

--- src/analysis/scratch_multiorganelle_decon.py
+++ src/analysis/scratch_multiorganelle_decon.py
@@ -11,6 +11,18 @@ from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 from scipy.stats import ttest_ind
 
+### The aim of the code is as follows: In microscopy, often times we are limited by the number of channels we can
+###  use to separate out our organelles of interest. This could be due to the number of channels available on the microscope,
+###  due to the number of channels we can use without causing too much phototoxicity or photobleaching, or due to
+###  the number of channels we can use without causing too much spectral overlap. In order to get around this, we
+###  can create a deconvolution algorithm to post-hoc separate out organelles of interest from a single channel of an
+###  image. Our organelle feature extraction pipeline extracts a wide variety of both morphology and motility features.
+###  We can use these features to train a machine learning model on each individual organelle first, then use the
+###  trained model to predict the organelle for each branch in a segmented image. One can imagine running this type of
+###  model on, for example 3 organelles in 4 different channels each, resulting in useful feature data for 12 organelles
+###  in a single timelapse. Additionally, we run this entire pipeline on only 3 timepoints, allowing just enough frames
+###  to extract out acceleration features, and we are still able to create a highly performant model. Ideally, one would
+###  coordinate the same-channel organelles to be as different as possible to allow for the best model performance.
 
 top_dir = r"D:\test_files\nelly_multichannel"
 # get all non-folder files
@@ -18,24 +30,29 @@ all_files = os.listdir(top_dir)
 all_files = [os.path.join(top_dir, file) for file in all_files if not os.path.isdir(os.path.join(top_dir, file))]
 
 label_types = ['organelle', 'branch']
-# label_types = ['branch']
+
+# we also want to test the model's performance on morphology data alone, motility data alone, and the combination
+#  theoretically, if motility and morphology are both important, our combo model will outperform the individual models.
 df_types = ['combo', 'motility', 'morphology']
-# df_types = ['combo', 'morphology']
 
 for label_type in label_types:
     for df_type_to_use in df_types:
-        # generate a random guess line
+        # generate a random guess line for plotting purposes
         r_x, r_y = [0, 1], [0, 1]
         plt.figure(figsize=(10, 10))
         reports = []
         fprs = []
         tprs = []
         roc_aucs = []
-        # for file_to_leave_out in range(2):
+
+        # we have 11 files, so we will leave out one file at a time and train on the rest,
+        #  then test on the left out file
+        #  we will average the results from all 11 tests to get the final result
         for file_to_leave_out in range(len(all_files)):
             print(f'Leaving out file {file_to_leave_out + 1} of {len(all_files)}')
             test_file = all_files[file_to_leave_out]
 
+            # get the dataframes for the 1 test file
             test_iminfo = ImInfo(test_file, ch=-1)
             test_motility_df = pd.read_csv(test_iminfo.pipeline_paths[f'{label_type}_motility_features'])
             test_motility_df.reset_index(drop=True, inplace=True)
@@ -43,22 +60,25 @@ for label_type in label_types:
             test_label_morphology_df.reset_index(drop=True, inplace=True)
             test_branch_morphology_df = pd.read_csv(test_iminfo.pipeline_paths[f'{label_type}_skeleton_features'])
             test_branch_morphology_df.reset_index(drop=True, inplace=True)
-
+            # combine the label and branch morphology dataframes to create the final morphology dataframe
             test_combo_morphology_df = pd.concat([test_label_morphology_df, test_branch_morphology_df], axis=1)
-
+            # combine the morphology and motility dataframes to create the final combo dataframe
             test_combo_df = pd.concat([test_combo_morphology_df, test_motility_df], axis=1)
 
+            # our files are split as follows: ch0 = mitochondria, ch1 = golgi
+            # to get the ground truth, we will use the label image from each individual channel image
+            #  and look at the overlap with the label image from the artificially combined multi-organelle image
             test_ch0_gt = ImInfo(test_file, ch=0)
             test_ch1_gt = ImInfo(test_file, ch=1)
 
-            # get label image, frame 1 from multichannel file
-            # get the label image from the multichannel file
             if label_type == 'organelle':
                 pipeline_path = 'im_instance_label'
             elif label_type == 'branch':
                 pipeline_path = 'im_skel_relabelled'
             else:
                 raise ValueError(f'Invalid label_type: {label_type}')
+
+            # get the label mask from the artificially combined multi-organelle image
             test_all_labels = test_iminfo.get_im_memmap(test_iminfo.pipeline_paths[pipeline_path])[1]
             # get the label mask from the single channel files
             test_ch0_gt_mask = test_ch0_gt.get_im_memmap(test_ch0_gt.pipeline_paths[pipeline_path])[1]>0
@@ -67,8 +87,8 @@ for label_type in label_types:
             gt_ch = []
 
             non_0_all_regions = skimage.measure.regionprops(test_all_labels)
-            # if a branch has more overlap with mitochondria mask, its a mitochondria (ch0)
-            # if a branch has more overlap with golgi mask, its a golgi (ch1)
+            # if a branch regionprop has more overlap with mitochondria mask, it's a mitochondria (ch0)
+            # if a branch regionprop has more overlap with golgi mask, it's a golgi (ch1)
             # this is our ground truth
             for region_num, region in enumerate(non_0_all_regions):
                 ch0_sum_px = np.sum(test_ch0_gt_mask[region.coords[:, 0], region.coords[:, 1], region.coords[:, 2]])
@@ -97,6 +117,7 @@ for label_type in label_types:
             test_df_to_use.reset_index(drop=True, inplace=True)
             og_test_df = test_df_to_use.copy()
 
+            # get the dataframes for the 10 training files
             train_motility_df = pd.DataFrame()
             train_label_morphology_df = pd.DataFrame()
             train_branch_morphology_df = pd.DataFrame()
@@ -124,7 +145,6 @@ for label_type in label_types:
             train_combo_morphology_df = pd.concat([train_label_morphology_df, train_branch_morphology_df], axis=1)
             train_combo_df = pd.concat([train_combo_morphology_df, train_motility_df], axis=1)
 
-
             if df_type_to_use == 'combo':
                 train_df_to_use = train_combo_df.copy()
             elif df_type_to_use == 'motility':
@@ -143,12 +163,16 @@ for label_type in label_types:
 
             og_train_df = train_df_to_use.copy()
 
+            # these columns are not useful for training, since they are either the ground truth or are features that
+            #  may vary between experiments due to imaging conditions rather than inherent organelle properties.
             remove_cols = [
                 'main_label', 'file', 'label', 't', 'gt_ch',
                 'intensity_mean', 'intensity_std', 'intensity_range',
                 'frangi_mean', 'frangi_std', 'frangi_range',
             ]
 
+            # empirically, these motility features are the useful ones for this purpose, others seem to decrease
+            #  performance of the model.
             stats_to_use = ['median', 'max', 'std', 'min']
             features_to_use = ['rel_ang_vel_mag_12', 'rel_ang_acc_mag',
                                'rel_lin_vel_mag_12', 'rel_lin_acc_mag',
@@ -214,7 +238,7 @@ for label_type in label_types:
             # plt.savefig(os.path.join(top_dir, 'output', f'volcano_plot-{label_type}-{df_type_to_use}-{file_to_leave_out}.png'), dpi=500)
             # plt.close()
 
-            #shuffle the training dataset and og dataset in the same way
+            #shuffle the training dataset and original dataset in the same way
             train_df_to_use = train_df_to_use.sample(frac=1, random_state=42)
             og_train_df = og_train_df.iloc[train_df_to_use.index]
 
@@ -224,13 +248,13 @@ for label_type in label_types:
             test_standardized_features = scaler.transform(test_df_to_use)
             test_standardized_features = pd.DataFrame(test_standardized_features, columns=test_df_to_use.columns)
 
-            # test the model on the test file, have it generate a vector assigning branches to the golgi channel or mito channel (0 or 1)
+            # train a random forest classifier on the training files
             clf = RandomForestClassifier(n_estimators=300, random_state=42, n_jobs=-1)
             clf.fit(train_standardized_features, og_train_df['gt_ch'])
+            # test the model on the test file, have it generate a vector assigning branches to the golgi channel or mito channel (0 or 1)
             y_pred = clf.predict(test_standardized_features)
 
-            # Evaluate the model with cross-validation
-            # compare the vector to the ground truth for metrics
+            # evaluate the model: compare the vector to the ground truth for metrics
             report = classification_report(og_test_df['gt_ch'], y_pred, output_dict=True)
 
             # get roc curve
