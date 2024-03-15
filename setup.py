f194fd7949875ec95650af881805fdcd99e826ab
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-03-15 15:48:26 -0700

    Napari plugin is functional on mac

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nelly-organelles',
-    version='0.1.0',
+    name='nellie',
+    version='0.0.1',
     packages=find_packages(),
     description='woaaaah',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Austin E. Y. T. Lefebvre',
     author_email='austin.e.lefebvre@gmail.com',
-    url='https://github.com/aelefebv/nelly',
+    url='https://github.com/aelefebv/nellie',
     install_requires=[
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.12',
 )
