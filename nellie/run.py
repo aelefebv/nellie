8d403b57079445df18a148ea315accc8853c7964
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2024-03-20 02:14:32 -0700

    Added all_tracks_for_label section for generating and saving multi-frame tracks

--- nellie/run.py
+++ nellie/run.py
@@ -36,18 +36,19 @@ def run(im_path, num_t=None, remove_edges=True, ch=0):
 
 
 if __name__ == "__main__":
-    ## Single file run
-    im_path = r"D:\test_files\nelly_tests_2d3d\test.ome.tif"
-    im_info = run(im_path, remove_edges=False, ch=0, num_t=3)
+    # Single file run
+    im_path = r"D:\test_files\nellie_longer_smorgasbord\deskewed-peroxisome.ome.tif"
+    im_info = run(im_path, remove_edges=True, ch=0)
 
-    # ## Directory bactch run
+    # # Directory bactch run
     # import os
-    # top_dir = r"D:\test_files\gav_aging_2"
-    # ch = 1
+    # top_dir = r"D:\test_files\nellie_longer_smorgasbord"
+    # ch = 0
+    # num_t = None
     # # get all non-folder files
     # all_files = os.listdir(top_dir)
     # all_files = [os.path.join(top_dir, file) for file in all_files if not os.path.isdir(os.path.join(top_dir, file))]
     # for file_num, tif_file in enumerate(all_files):
     #     # for ch in range(1):
     #     print(f'Processing file {file_num + 1} of {len(all_files)}, channel {ch + 1} of 1')
-    #     im_info = run(tif_file, remove_edges=True, ch=ch)
+    #     im_info = run(tif_file, remove_edges=True, ch=ch, num_t=num_t)
--- nellie/tracking/all_tracks_for_label.py
+++ nellie/tracking/all_tracks_for_label.py
@@ -10,7 +10,7 @@ class LabelTracks:
         self.im_info = im_info
         self.num_t = num_t
         if label_im_path is None:
-            label_im_path = self.im_info.pipeline_paths['im_instance_label_reassigned']
+            label_im_path = self.im_info.pipeline_paths['im_instance_label']
         self.label_im_path = label_im_path
 
         if num_t is None:
@@ -22,13 +22,16 @@ class LabelTracks:
         self.im_memmap = self.im_info.get_im_memmap(self.im_info.im_path)
         self.im_memmap = get_reshaped_image(self.im_memmap, im_info=self.im_info)
 
-    def run(self, label_num=11, start_frame=0, end_frame=None, min_track_num=0, skip_coords=1, max_distance_um=0.5):
+    def run(self, label_num=None, start_frame=0, end_frame=None, min_track_num=0, skip_coords=1, max_distance_um=0.5):
         if end_frame is None:
             end_frame = self.num_t
         num_frames = self.label_memmap.shape[0] - 1
         if start_frame > num_frames:
             return [], {}
-        coords = np.argwhere(self.label_memmap[start_frame] == label_num).astype(float)
+        if label_num is None:
+            coords = np.argwhere(self.label_memmap[start_frame] > 0).astype(float)
+        else:
+            coords = np.argwhere(self.label_memmap[start_frame] == label_num).astype(float)
         if coords.shape[0] == 0:
             return [], {}
         coords = np.array(coords[::skip_coords])
@@ -57,3 +60,45 @@ class LabelTracks:
                 for property in track_properties_bw.keys():
                     track_properties[property] = track_properties_bw[property] + track_properties[property]
         return tracks, track_properties
+
+
+if __name__ == "__main__":
+    im_path = r"D:\test_files\nellie_longer_smorgasbord\deskewed-peroxisome.ome.tif"
+    im_info = ImInfo(im_path, ch=0)
+    num_t = 20
+    label_tracks = LabelTracks(im_info, num_t=num_t)
+    label_tracks.initialize()
+    # tracks, track_properties = label_tracks.run(label_num=None, skip_coords=1)
+
+    all_tracks = []
+    all_props = {}
+    max_track_num = 0
+    for frame in range(num_t):
+        tracks, track_properties = label_tracks.run(label_num=None, start_frame=frame, end_frame=None,
+                                                    min_track_num=max_track_num,
+                                                    skip_coords=100)
+        all_tracks += tracks
+        for property in track_properties.keys():
+            if property not in all_props.keys():
+                all_props[property] = []
+            all_props[property] += track_properties[property]
+        if len(tracks) == 0:
+            break
+        max_track_num = max([track[0] for track in tracks]) + 1
+
+    # pickle tracks and track properties
+    import pickle
+    import datetime
+    dt = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    with open(f'{dt}-mt_tracks.pkl', 'wb') as f:
+        pickle.dump(all_tracks, f)
+    with open(f'{dt}-mt_props.pkl', 'wb') as f:
+        pickle.dump(all_props, f)
+
+    # import napari
+    # viewer = napari.Viewer()
+    #
+    # raw_im = im_info.get_im_memmap(im_info.im_path)[:num_t]
+    # viewer.add_image(raw_im, name='raw_im')
+    # viewer.add_tracks(all_tracks, properties=all_props, name='tracks')
+    # napari.run()
