ac28a336a78f372e2156744990caf8a9e4e19e4f
imageanalysis <imageanalysis@HPDEMO-W11.calicolabs.local>
2024-07-07 11:23:27 -0700

    final woods hole save

--- nellie/run.py
+++ nellie/run.py
@@ -10,7 +10,6 @@ from nellie.tracking.voxel_reassignment import VoxelReassigner
 
 def run(im_path, num_t=None, remove_edges=True, ch=0, output_dirpath=None, otsu_thresh_intensity=False, dim_sizes=None, threshold=None, dim_order=None):
     im_info = ImInfo(im_path, ch=ch, output_dirpath=output_dirpath, dim_sizes=dim_sizes, dimension_order=dim_order)
-
     preprocessing = Filter(im_info, num_t, remove_edges=remove_edges)
     preprocessing.run()
 
@@ -36,21 +35,21 @@ def run(im_path, num_t=None, remove_edges=True, ch=0, output_dirpath=None, otsu_
 
 
 if __name__ == "__main__":
-    # Single file run
-    # im_path = r"/Users/austin/Downloads/test.tif"
-    # im_path = r"F:\ProbabilityMaps_ExM\Sample1.tif"
-    # im_info = run(im_path, remove_edges=False, ch=0)
-    # im_info = run(im_path, remove_edges=False, ch=0, dim_sizes={'T': 1, 'Z': 1, 'Y': 0.0605, 'X': 0.0605}, otsu_thresh_intensity=True)
-    # im_info = run(im_path, remove_edges=False, ch=0, dim_sizes={'T': 1, 'Z': 1, 'Y': 0.10833, 'X': 0.10833})
-    # im_info = run(im_path, remove_edges=False, ch=1, dim_sizes={'T': 1, 'Z': 0.3/4, 'Y': 0.17/4, 'X': 0.17/4}, dim_order='CZYX', threshold=0.5)
+    # # Single file run
+    # # im_path = r"/Users/austin/Downloads/test.tif"
+    im_path = r"C:\Users\imageanalysis\Downloads\240531_dsDNA-TOM20-siMCU2_005_decon.ome.tiff"
+    # # im_info = run(im_path, remove_edges=False, ch=0)
+    # # im_info = run(im_path, remove_edges=False, ch=0, dim_sizes={'T': 1, 'Z': 1, 'Y': 0.0605, 'X': 0.0605}, otsu_thresh_intensity=True)
+    # im_info = run(im_path, remove_edges=False, ch=1)
+    im_info = run(im_path, remove_edges=False, ch=1, dim_sizes={'T': 1, 'Z': 0.1, 'Y': 0.1, 'X': 0.1}, otsu_thresh_intensity=True)
 
-    import os
-    top_dir = r"F:\ProbabilityMaps_ExM"
-    all_tifs = os.listdir(top_dir)
-    all_tifs = [os.path.join(top_dir, file) for file in all_tifs if file.endswith('.tif')]
-    for tif_file in all_tifs:
-        print(f'Processing {tif_file}')
-        im_info = run(tif_file, remove_edges=False, ch=1, dim_sizes={'T': 1, 'Z': 0.3/4, 'Y': 0.17/4, 'X': 0.17/4}, dim_order='CZYX', threshold=0.5)
+    # import os
+    # top_dir = r"F:\fert"
+    # all_tifs = os.listdir(top_dir)
+    # all_tifs = [os.path.join(top_dir, file) for file in all_tifs if file.endswith('.tif')]
+    # for tif_file in all_tifs:
+    #     print(f'Processing {tif_file}')
+    #     im_info = run(tif_file, remove_edges=False, ch=0)
 
     # Directory bactch run
     # import os
