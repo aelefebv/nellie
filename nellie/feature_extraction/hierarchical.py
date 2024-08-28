e3ffbdebf37d3488306b2d89d99526d0faae9f20
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-08-28 11:17:13 -0700

    path

--- nellie/feature_extraction/hierarchical.py
+++ nellie/feature_extraction/hierarchical.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy import spatial
 from skimage.measure import regionprops
 
-from . import logger
+import logger
 from im_info.verifier import ImInfo
 from tracking.flow_interpolation import FlowInterpolator
 import pandas as pd
