f70c673cae2c4cb74c0eadbb4e39466391929c10
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-08-28 11:07:02 -0700

    pathing

--- nellie/run.py
+++ nellie/run.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import multiprocessing
 
 from nellie.feature_extraction.hierarchical import Hierarchy
