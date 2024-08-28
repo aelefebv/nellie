88f0feab32261e83f6c393b271f3cf7c23aa482b
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-08-28 11:08:28 -0700

    path

--- nellie/run.py
+++ nellie/run.py
@@ -1,7 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+print(os.path.dirname(os.path.abspath(__file__)))
 import multiprocessing
 
 from nellie.feature_extraction.hierarchical import Hierarchy
