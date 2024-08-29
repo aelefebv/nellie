6b90e7551d072b6d3a827ad4555e8497f8f3b818
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-08-29 00:53:56 -0700

    Fix for new axes

--- scripts/cpp120.py
+++ scripts/cpp120.py
@@ -4,7 +4,7 @@ from nellie.run import run_all_directories_parallel
 
 top_dir = '/scratch3/prateek/isr_animate/20240315_CPPX120_ISR_Washout/84a5de3a-23c1-4ce0-8797-f4a647407b89/images'
 substring = 'ch04'
-output_dir = '/scratch3/austin/projects/isr_animate_2/20240315_CPPX120_ISR_Washout'
+output_dir = '/scratch3/austin/projects/isr_animate_4/20240315_CPPX120_ISR_Washout'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 nellie_needed_dir = os.path.join(output_dir, 'nellie_necessities')
