6094901c5cd7b03f3ee9c1021ada1e5e9572ebb9
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-10-04 11:39:51 -0700

    sg

---
+++ scripts/cpp114_sg.py
@@ -0,0 +1,16 @@
+import os
+
+from nellie.run import run_all_directories_parallel
+
+import datetime
+
+dt = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+top_dir = '/scratch3/prateek/isr_animate/20231215_CPPX114_ISR_Washout/hs/61fd3e14-937f-4274-9f0f-06b678605ae7/images'
+substring = 'ch02'
+output_dir = f'/scratch3/austin/projects/isr_animate/{substring}-low_frob-{dt}-20231215_CPPX114_ISR_Washout'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+nellie_needed_dir = os.path.join(output_dir, 'nellie_necessities')
+if not os.path.exists(nellie_needed_dir):
+    os.makedirs(nellie_needed_dir)
+run_all_directories_parallel(top_dir, substring, output_dir)
