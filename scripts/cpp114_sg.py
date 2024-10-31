d5f7784ffd0f19747c2c62f2f53a124c6deb8b37
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-10-31 11:20:54 -0700

    sg

--- scripts/cpp114_sg.py
+++ scripts/cpp114_sg.py
@@ -5,12 +5,12 @@ from nellie.run import run_all_directories_parallel
 import datetime
 
 dt = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-top_dir = '/scratch3/prateek/isr_animate/20231215_CPPX114_ISR_Washout/hs/61fd3e14-937f-4274-9f0f-06b678605ae7/images'
+top_dir = '/Users/austin/test_files/nellie_cpp_sg_test'
 substring = 'ch02'
-output_dir = f'/scratch3/austin/projects/isr_animate/{substring}-low_frob-{dt}-20231215_CPPX114_ISR_Washout'
+output_dir = f'/Users/austin/test_files/out_sg_test'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 nellie_needed_dir = os.path.join(output_dir, 'nellie_necessities')
 if not os.path.exists(nellie_needed_dir):
     os.makedirs(nellie_needed_dir)
-run_all_directories_parallel(top_dir, substring, output_dir)
+run_all_directories_parallel(top_dir, substring, output_dir, mp=False)
