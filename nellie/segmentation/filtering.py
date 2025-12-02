1d136cfe1bbb04d2db038d11dc93dd1435918023
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2025-12-01 16:03:56 -0800

    remove docs

--- nellie/segmentation/filtering.py
+++ nellie/segmentation/filtering.py
@@ -2,11 +2,35 @@ from itertools import combinations_with_replacement  # kept for compatibility, m
 
 from nellie import logger
 from nellie.im_info.verifier import ImInfo
-from nellie.utils.general import bbox
 import numpy as np
 from nellie import ndi, xp, device_type
 
 from nellie.utils.gpu_functions import triangle_threshold, otsu_threshold
+
+def bbox(im):
+    if len(im.shape) == 2:
+        rows = xp.any(im, axis=1)
+        cols = xp.any(im, axis=0)
+        if (not rows.any()) or (not cols.any()):
+            return 0, 0, 0, 0
+        rmin, rmax = xp.where(rows)[0][[0, -1]]
+        cmin, cmax = xp.where(cols)[0][[0, -1]]
+        return int(rmin), int(rmax), int(cmin), int(cmax)
+
+    elif len(im.shape) == 3:
+        r = xp.any(im, axis=(1, 2))
+        c = xp.any(im, axis=(0, 2))
+        z = xp.any(im, axis=(0, 1))
+        if (not r.any()) or (not c.any()) or (not z.any()):
+            return 0, 0, 0, 0, 0, 0
+        rmin, rmax = xp.where(r)[0][[0, -1]]
+        cmin, cmax = xp.where(c)[0][[0, -1]]
+        zmin, zmax = xp.where(z)[0][[0, -1]]
+        return int(rmin), int(rmax), int(cmin), int(cmax), int(zmin), int(zmax)
+
+    else:
+        print("Image not 2D or 3D... Cannot get bounding box.")
+        return None
 
 
 class Filter:
